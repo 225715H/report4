2e2d6d1ee35d2635e52365711a6c3a14569f0e79
Motoki Higa <e225715@ie.u-ryukyu.ac.jp>
2022-11-30 02:00:40 +0900

    step2

--- app/src/main/java/jp/ac/uryukyu/ie/e225715/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e225715/Enemy.java
@@ -12,6 +12,7 @@ public class Enemy extends LivingThing{
     public Enemy(String name, int maximumHP, int attack){
         super(name, maximumHP, attack);
     }
+    
     @Override
     public void wounded(int damage){
         hitPoint -= damage;
---
+++ app/src/test/java/jp/ac/uryukyu/ie/e225715/WarriorTest.java
@@ -0,0 +1,21 @@
+package jp.ac.uryukyu.ie.e225715;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class WarriorTest {
+    @Test
+    void attackTest(){
+        int defaultWarriorAttack = 100;
+        int defaultEnemyHP = 200;
+        for (int i = 0; i < 5; i++){
+            Warrior demoWarrior = new Warrior("デモ戦士", 100, defaultWarriorAttack);
+            Enemy demoEnemy = new Enemy("デモスライム", 200, 10);
+            demoWarrior.attackWithWeponSkill(demoEnemy);
+            int damage = defaultEnemyHP - demoEnemy.getHitPoint();
+            assertEquals(defaultWarriorAttack * 1.5, damage);
+        }
+    }
+    
+
+}
